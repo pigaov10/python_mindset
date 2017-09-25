eb2aef78cc195d001495386df76c23fb832e81a1
Raphael Iarussi <pigaov10@gmail.com>
2017-09-25 16:25:18 -0300

    Create working_with_zip.py

---
+++ working_with_zip.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+scientists = ['Albert Einstein', 'Isaac Newton', 'Nikola Tesla']
+
+theory = ['Relativity', 'Gravity', 'Electromagnetism']
+
+theory_scientists = dict(zip(scientists, theory))
+
+print(theory_scientists)
